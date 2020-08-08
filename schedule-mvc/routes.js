b23c472581e722981905499ea66bf71e368792de
Demys Cota <demys.cota@snowmanlabs.com>
2020-08-08 13:19:07 -0300

    form login and signup

--- schedule-mvc/routes.js
+++ schedule-mvc/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
 
 // Rotas da home
 route.get('/', homeController.paginaInicial);
 route.post('/', homeController.trataPost);
+
+route.get('/login', loginController.index)
 
 module.exports = route;
--- schedule-mvc/src/middlewares/middleware.js
+++ schedule-mvc/src/middlewares/middleware.js
@@ -11,6 +11,7 @@ exports.checkCsrfError = (err, req, res, next) => {
   if(err && 'EBADCSRFTOKEN' === err.code) {
     return res.render('404');
   }
+  next()
 };
 
 exports.csrfMiddleware = (req, res, next) => {
