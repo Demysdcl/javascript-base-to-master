58dc8f4a7c1eccbd0553a5a03c7edd77b789f6eb
Demys Cota <demys.cota@snowmanlabs.com>
2020-08-20 17:35:16 -0300

    validate form

--- schedule-mvc/frontend/main.js
+++ schedule-mvc/frontend/main.js
@@ -1,3 +1,7 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
+import LoginValidator from "./modules/LoginValidator";
+
+new LoginValidator(".form-register").init();
+new LoginValidator(".form-login").init();
