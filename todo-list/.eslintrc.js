5d41828630743416fd4491c5109b6c32a0ca3e61
Demys Cota <demys.cota@snowmanlabs.com>
2020-08-30 16:13:37 -0300

    config eslint

---
+++ todo-list/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: 'babel-lint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -6,7 +6,6 @@ function App() {
     <div className="App">
       <h1>Hello world!</h1>
     </div>
-  );
+  )
 }
-
 export default App;
