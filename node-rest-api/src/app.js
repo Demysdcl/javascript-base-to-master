4235fdf3333d2bef5e843f58f4ede67d8c4b39d4
Demys Cota <demys.cota@snowmanlabs.com>
2020-08-29 21:41:15 -0300

    finish build

--- node-rest-api/src/app.js
+++ node-rest-api/src/app.js
@@ -21,7 +21,7 @@ class App {
   middlewares() {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
-    this.app.use(express.static(resolve(__dirname, 'uploads', 'images')));
+    this.app.use(express.static(resolve(__dirname, 'uploads')));
   }
 
   routes() {
