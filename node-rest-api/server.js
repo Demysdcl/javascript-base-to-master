a2a72a4a45814e5dbdc4a5b23efcde6d1a8f4785
Demys Cota <demys.cota@snowmanlabs.com>
2020-08-29 21:20:48 -0300

    student service and url file

--- node-rest-api/server.js
+++
@@ -1,9 +0,0 @@
-import app from './app';
-
-const port = 3001;
-app.listen(port, () => {
-  console.log();
-  console.log(`Listening port ${port}`);
-  console.log(`CTRL + Click in http://localhost:${port}`);
-  console.log('CTRL + c to finish the application');
-});
