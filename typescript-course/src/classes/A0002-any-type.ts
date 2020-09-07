0cdb30471400b92f5803ad153d722c93c3301419
Demys Cota <demys.cota@snowmanlabs.com>
2020-09-07 11:12:12 -0300

    init typescript class

---
+++ typescript-course/src/classes/A0002-any-type.ts
@@ -0,0 +1,8 @@
+function printMessage(text: unknown) {
+  console.log(text)
+}
+
+printMessage([1, 2, 4])
+printMessage('a')
+printMessage(1)
+printMessage({ name: 'Demys', email: 'demysdcl@gmail.com' })
