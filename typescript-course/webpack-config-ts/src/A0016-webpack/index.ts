e1062b7ca29f5d4a215f86f1368dbc0b94f8ee71
Demys Cota <demys.cota@snowmanlabs.com>
2020-09-08 09:40:37 -0300

    validation

--- typescript-course/webpack-config-ts/src/A0016-webpack/index.ts
+++ typescript-course/webpack-config-ts/src/A0016-webpack/index.ts
@@ -10,35 +10,49 @@ const password2 = document.querySelector('.password2') as HTMLInputElement
 
 form.addEventListener('submit', (event: Event) => {
   event.preventDefault()
-  hideErrorMessage(form)
-  checkForEmptyFields(username, email, password, password2)
-  checkEmail()
-  checkPasswords()
+  hideErrorMessage()
+  if (shouldSendForm()) alert('The form was sent')
 })
 
-function checkForEmptyFields(...inputs: HTMLInputElement[]) {
+function shouldSendForm() {
+  const hasNoEmptyFields = checkForEmptyFields(username, email, password, password2)
+  const isValidEmail = checkEmail()
+  const passwodsMatch = checkPasswords()
+  return hasNoEmptyFields && isValidEmail && passwodsMatch
+}
+
+function checkForEmptyFields(...inputs: HTMLInputElement[]): boolean {
+  let hasError = false
   inputs.forEach((input) => {
     const label = input.previousElementSibling as HTMLLabelElement
     if (!input.value) {
       showErrorMessage(input, `${label.innerText} is required`)
+      hasError = true
     }
   })
+  return !hasError
 }
 
 function checkEmail() {
+  let hasError = false
   if (!isEmail(email.value)) {
     showErrorMessage(email, 'The e-mail is invalid')
+    hasError = true
   }
+  return !hasError
 }
 
 function checkPasswords() {
+  let hasError = false
   if (password.value !== password2.value) {
     showErrorMessage(password, 'The passwords not match')
     showErrorMessage(password2, 'The passwords not match')
+    hasError = true
   }
+  return !hasError
 }
 
-function hideErrorMessage(form: HTMLFormElement) {
+function hideErrorMessage() {
   form
     .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
     .forEach((item) => item.classList.remove(SHOW_ERROR_MESSAGES))
@@ -50,6 +64,3 @@ function showErrorMessage(input: HTMLInputElement, msg: string) {
   errorMessage.innerText = msg
   formFields.classList.add(SHOW_ERROR_MESSAGES)
 }
-
-showErrorMessage(username, 'Whats the matter')
-hideErrorMessage(form)
