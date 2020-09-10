b18e0b26a167962a3af597583461d78806ba038b
Demys Cota <demys.cota@snowmanlabs.com>
2020-09-10 08:50:39 -0300

    config new project

--- classes-typecript/src/A0021 - classes/A0032-type-alias.ts
+++ classes-typecript/src/A0021 - classes/A0032-type-alias.ts
@@ -8,6 +8,8 @@ type EmployeeType = {
   position: string
 }
 
+type FullPersonType = PersonType & EmployeeType
+
 export class Person implements PersonType, EmployeeType {
   constructor(
     public name: string,
@@ -20,7 +22,23 @@ export class Person implements PersonType, EmployeeType {
   }
 }
 
+export class Person2 implements FullPersonType {
+  constructor(
+    public name: string,
+    public lastname: string,
+    public position: string,
+  ) {}
+
+  fullname(): string {
+    return this.name + ' ' + this.lastname
+  }
+}
+
 const person = new Person('Demys', 'Lima', 'Dev')
+const person2 = new Person2('Demys2', 'Lima', 'Dev')
 
 console.log(person)
 console.log(person.fullname())
+
+console.log(person2)
+console.log(person2.fullname())
